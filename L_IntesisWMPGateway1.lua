b6ad40ae2e603ab8fa37c0deafed94c2d4295ade
IkaDeady <Falves@ikatu.com>
2018-11-15 11:56:14 -0300

    driver development guide

--- L_IntesisWMPGateway1.lua
+++ L_IntesisWMPGateway1.lua
@@ -13,6 +13,10 @@ Universal IntesisBox’s are developed to offer compatibility with thousands of 
 
 This IntesisBox driver for BLGW will work with all models of IntesisBox and will provide full two way control/feedback.
 
+
+Khimo support notes:
+  - Plese don't let empty tables on commands or states
+
 ]]
 
 driver_channels={
@@ -43,9 +47,9 @@ local t2spStates = {
   	["HEAT SP"]= { arguments= { temperatureArgument("Temperature", "C", 22.0 ) } },
   	["COOL SP"]= { arguments= { temperatureArgument("Temperature", "C", 22.0 ) } },
   	["MODE"]= { arguments= { enumArgument("MODE", {  "Off", "Heat", "Cool", "Auto", "_Dry", "_Fan"  }, "Auto" ) } },
-    ["FAN AUTO"]= {},
-    ["_ONOFF"]= { arguments= { enumArgument("ON / OFF", {  true, false  }, false ) } },
-    ["_FANSP"]= { arguments= { enumArgument("Fan Speed", {  "Auto", "_High", "_Med", "_low", "_Quiet" }, "Auto" ) } },
+    	["FAN AUTO"]= {},
+    	["_ONOFF"]= { arguments= { enumArgument("ON / OFF", {  true, false  }, false ) } },
+    	["_FANSP"]= { arguments= { enumArgument("Fan Speed", {  "Auto", "_High", "_Med", "_low", "_Quiet" }, "Auto" ) } },
   	["_VANEUD"]= { arguments= { enumArgument("VANEUD", {  1, 2, 3, 4, 5, 6, "_SWING" }, 1 ) } },
   	["_VANELR"]= { arguments= { enumArgument("VANELR", {  1, 2, 3, 4, 5, "_SWING" }, 1 ) } },
   	["_SWING"]= { arguments= { enumArgument("SWING", {  "_UD", "_LR", "_UDLR", "Off" }, "Off" ) } }
@@ -53,10 +57,52 @@ local t2spStates = {
 
 resource_types= {
   ["Intesis Thermostat"]= { 
-  	standardResourceType= "THERMOSTAT_2SP",
+    standardResourceType= "THERMOSTAT_2SP",
     address= theAddress,
     events= t2spActions,
     commands= t2spActions,
     states= t2spStates 
   }
 }
+
+[[
+* The process is a global function responsible of control the message channel, receive and send states requests to mantain
+the connection alive, there you will set the state of your resources.
+This function is called on driver init and is called in a loop while the system is loaded
+
+function process()
+  Trace('Starting process') --The Trace command let you print process function outputs on the BLI/BLGW log
+  -- You need to send some request to your device to know if you can reach it, 
+  -- for that I recommend to send a state request which also let you set the initial state of the devices
+  channel.write('GET,acNum:function/ CHN,acNum:function,value') -- You will read and write through the channel
+  local reader, msg = channel.readUntil('expected message', <timeout>) 
+  if reader == CONST.OK and <some condition> then --CONST.OK is equivalent to a 200 http response
+    driver.setOnline() -- Will turn green the driver label
+    setResourceState('resource.typeId', {address=resource.address}, {['state_name'] = newState}) -- Update resource state
+    - There exists to ways to keep the driver alive, use channel.status() in a while loop or let the process function end and starts again
+    while (channel.status()) do 
+      -- wait for some state coming 
+    end
+  else
+    -- set driver label to red and return error
+    driver.setError()
+    return CONST.HW_ERROR
+  end
+  -- Print a message and wait for some time till start the process again
+  channel.retry('End of the process', <polling time>)
+  return CONST.POLLING
+end
+
+
+* executeCommand runs on parallel to the process and is the responsible to execute the commands that comes from the macros
+  
+function executeCommand(command, resource, commandArgs)
+  if resource.typeId == "some resource type" and command == "some command" then
+    channel.write("SET,acNum:function,value / LIMITS:function,range") -- The feedback from the commands is captured on the process function
+  else if -- the remaining commands
+  end
+end
+  
+The two functions above are mandatories to the driver works, 
+respecting these functions you can define how much functions as you need always as local functions
+]]
